d42d9e16b9f590843f64322b25664944900852a8
mimibattistella <milena.battistella.ribeiro@escola.pr.gov.br>
2025-09-19 11:36:12 -0300

    Coloquei o placeholder de login (não funcional)

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,35 +1,94 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function LoginScreen() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.header}>
+        <Text style={styles.title}>GameUp</Text>
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Email</Text>
+        <View style={styles.input}>
+          <Text style={styles.placeholderTextLarge}></Text>
+        </View>
+
+        <Text style={styles.label}>Senha</Text>
+        <View style={styles.input}>
+          <Text style={styles.placeholderTextSmall}></Text>
+        </View>
+
+        
+      </View>
+
+      <View style={styles.footer} />
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5D00CC', 
+    justifyContent: 'space-between',
+  },
+  header: {
+    backgroundColor: '#FFEB00', 
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: 'flex-start',
+  },
+  title: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  form: {
+    paddingHorizontal: 20,
+  },
+  label: {
+    color: '#FFEB00',
+    fontSize: 14, 
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'left', 
+  },
+  input: {
+    backgroundColor: '#FFEB00',
+    height: 40,
+    borderRadius: 5,
+    marginBottom: 20,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  placeholderTextLarge: {
+    color: '#666',
+    fontSize: 18,  
+    fontWeight: 'bold',
+    textAlign: 'left', 
+    marginTop: 5, 
+  },
+  placeholderTextSmall: {
+    color: '#666',
+    fontSize: 16,  
+    fontWeight: 'bold',
+    textAlign: 'left', 
+    marginTop: 5, 
+  },
+  button: {
+    backgroundColor: '#FFEB00',
+    borderRadius: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  footer: {
+    backgroundColor: '#FFEB00',
+    height: 40,
+  },
+});
